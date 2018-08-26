8e2b80269a3baea4c1b36836385a29db9b4f6fb3
Robert Klep <robert@klep.name>
2018-08-26 14:54:27 +0200

    Code cleanups

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Homey      = require('homey');
 const Transition = require('./lib/transition');
 const Parser     = require('./lib/parser');
+const camelize   = s => s.replace(/(_[a-z])/g, m => m[1].toUpperCase());
 
 module.exports = class TransitionsApp extends Homey.App {
 
@@ -11,64 +12,61 @@ module.exports = class TransitionsApp extends Homey.App {
     this.transitions = {};
 
     // Register triggers.
-    this.triggers = {
-      onTransitionStartedTrigger : new Homey.FlowCardTrigger('transition_started')
-                                            .register()
-                                            .registerRunListener(this.onTransitionStartedTrigger.bind(this)),
-      onTransitionChangedTrigger : new Homey.FlowCardTrigger('transition_changed')
-                                            .register()
-                                            .registerRunListener(this.onTransitionChangedTrigger.bind(this)),
-      onTransitionEndedTrigger   : new Homey.FlowCardTrigger('transition_ended')
-                                            .register()
-                                            .registerRunListener(this.onTransitionEndedTrigger.bind(this)),
-      onTransitionStoppedTrigger : new Homey.FlowCardTrigger('transition_stopped')
-                                            .register()
-                                            .registerRunListener(this.onTransitionStoppedTrigger.bind(this)),
-    };
+    this.triggers = {};
+    this.registerTrigger('transition_started');
+    this.registerTrigger('transition_changed');
+    this.registerTrigger('transition_ended');
+    this.registerTrigger('transition_stopped');
 
     // Register actions.
-    new Homey.FlowCardAction('start_transition_text')
-             .register()
-             .registerRunListener(this.onStartTransitionTextAction.bind(this));
-    new Homey.FlowCardAction('start_transition')
-             .register()
-             .registerRunListener(this.onStartTransitionAction.bind(this));
-    new Homey.FlowCardAction('stop_transition')
-             .register()
-             .registerRunListener(this.onStopTransitionAction.bind(this));
+    this.registerAction('start_transition_text');
+    this.registerAction('start_transition');
+    this.registerAction('stop_transition');
   }
 
-  async onTransitionStartedTrigger(args, state) {
+  registerTrigger(name) {
+    const method = camelize(name) + 'Trigger';
+    this.triggers[method] = new Homey.FlowCardTrigger(name).register()
+                                     .registerRunListener(this[method].bind(this));
+  }
+
+  registerAction(name) {
+    const method = camelize(name) + 'Action';
+    new Homey.FlowCardAction(name).register()
+             .registerRunListener(this[method].bind(this));
+  }
+
+  async transitionStartedTrigger(args, state) {
     if (args.name !== state.name) return false;
     this.log(`[TRIGGER] transition started: name = ${ args.name }, value = ${ state.value }`);
     return true;
   }
 
-  async onTransitionChangedTrigger(args, state) {
+  async transitionChangedTrigger(args, state) {
     if (args.name !== state.name) return false;
     this.log(`[TRIGGER] transition changed: name = ${ args.name }, value = ${ state.value }`);
     return true;
   }
 
-  async onTransitionEndedTrigger(args, state) {
+  async transitionEndedTrigger(args, state) {
     if (args.name !== state.name) return false;
     this.log(`[TRIGGER] transition ended  : name = ${ args.name }, value = ${ state.value }`);
     return true;
   }
 
-  async onTransitionStoppedTrigger(args, state) {
+  async transitionStoppedTrigger(args, state) {
     if (args.name !== state.name) return false;
     this.log(`[TRIGGER] transition stopped: name = ${ args.name }, value = ${ state.value }`);
     return true;
   }
 
-  async onStartTransitionTextAction(args, state) {
+  async startTransitionTextAction(args, state) {
     let newArgs  = Parser(args.value);
     newArgs.name = args.name;
-    return this.onStartTransitionAction(newArgs, state);
+    return this.startTransitionAction(newArgs, state);
   }
 
-  async onStartTransitionAction(args, state) {
+  async startTransitionAction(args, state) {
     this.log(`[ACTION]  transition created: args =`, args);
 
     // Validate arguments.
@@ -76,7 +74,7 @@ module.exports = class TransitionsApp extends Homey.App {
       throw Error('STEP_EXCEEDS_DURATION');
     }
 
-    if (args.step < 1) {
+    if (args.step < 0.5) {
       throw Error('STEP_TOO_SMALL');
     }
 
@@ -88,43 +86,29 @@ module.exports = class TransitionsApp extends Homey.App {
     // Create a new transition, set event handlers, and start it.
     this.transitions[args.name] = new Transition(args);
     this.transitions[args.name].on('start', value => {
-      this.onTransitionStart(args.name, value);
+      this.onTransitionEvent('Started', args.name, value);
     }).on('step', value => {
-      this.onTransitionStep(args.name, value);
-    }).on('stop', value => {
-      this.onTransitionStop(args.name, value);
+      this.onTransitionEvent('Changed', args.name, value);
     }).on('end', value => {
-      this.onTransitionEnd(args.name, value);
+      this.onTransitionEvent('Ended', args.name, value);
+    }).on('stop', value => {
+      this.onTransitionEvent('Stopped', args.name, value);
     }).start();
     return true;
   }
 
-  async onStopTransitionAction(args, state) {
+  async stopTransitionAction(args, state) {
     this.log(`[ACTION]  stopping transition: name = ${ args.name }`);
     if (args.name in this.transitions) {
       this.transitions[args.name].stop();
     }
   }
 
-  onTransitionStart(name, value) {
-    this.log(`[ACTION]  transition started: name = ${ name }, value = ${ value }`);
-    this.triggers.onTransitionStartedTrigger.trigger({ name, value }, { name, value });
-  }
-
-  onTransitionStep(name, value) {
-    this.log(`[ACTION]  transition changed: name = ${ name }, value = ${ value }`);
-    this.triggers.onTransitionChangedTrigger.trigger({ name, value }, { name, value });
-  }
-
-  onTransitionEnd(name, value) {
-    this.log(`[ACTION]  transition ended  : name = ${ name }, value = ${ value }`);
-    this.triggers.onTransitionEndedTrigger.trigger({ name, value }, { name, value });
-    delete this.transitions[name];
-  }
-
-  onTransitionStop(name, value) {
-    this.log(`[ACTION]  transition stopped: name = ${ name }, value = ${ value }`);
-    this.triggers.onTransitionStoppedTrigger.trigger({ name, value }, { name, value });
-    delete this.transitions[name];
+  onTransitionEvent(event, name, value, cleanup = false) {
+    this.log(`[ACTION]  transition ${ event.toLowerCase()}: name = ${ name }, value = ${ value }`);
+    this.triggers['transition' + event + 'Trigger'].trigger({ name, value }, { name, value });
+    if (cleanup) {
+      delete this.transitions[name];
+    }
   }
 }
